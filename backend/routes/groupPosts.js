72b95fb36c00de98972364545eb2f7d2e60840d1
Iszy <25048003@myrp.edu.sg>
2026-07-24 15:20:58 +0000

    Clear main branch

--- backend/routes/groupPosts.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const groupController = require('../controllers/groupController');
-
-router.post('/:id/replies', groupController.createReply);
-
-module.exports = router;
--- backend/routes/profile.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const profileController = require('../controllers/profileController');
-
-router.get('/:id', profileController.getProfileById);
-router.put('/:id', profileController.updateProfile);
-
-module.exports = router;
